f7a746d779ffa2cac9bf624a21648bccb6f3c631
Moin Virani <moin.virani@gmail.com>
2013-10-22 19:52:42 -0400

    Completed Exercise 7 and updated the students by 15%

--- exercise7.rb
+++ exercise7.rb
@@ -4,36 +4,46 @@ students = {
 	:cohort3 => 22
 }
 
-puts (students)
-
 
 # Question 1
-
 def show (hash)
-	hash.each do |k,v| puts "#{k}: #{v} students"
+	hash.each do |key,value| puts "#{key}: #{value} students"
 	end
 end
-
 show(students)
 puts "\n" # line break
+
+
 
 # Question 2 - adding a new key to existing hash
 
 students[:cohort4] = 43
-
 puts "New Bitmaker Labs Cohort Numbers"
 show(students)
 puts "\n"
 
+
 # Question 3 - cohort names with the keys method
-
 puts "Cohort Names"
-
-student_names = students.each_key do |k| puts k
+student_names = students.each_key do |key| puts key
 	end
 	puts "\n"
 
+
 # Question 4 - increasing cohort by 5%
+
+def increase_size(hash, multiply)
+	hash.each do |key,value|
+		hash[key] = ((value*multiply).to_i)
+	end
+end
+
+puts "The cohort has increased by 5%! Here are the new numbers"
+increase_size(students, 1.05)
+show (students)
+puts '\n'
+
+
 
 
 
